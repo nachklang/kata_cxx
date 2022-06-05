727de06858f68c16e2dc315d5593082ca9d7f350
Aleksey Myasnikov <myasnikov.alexej@gmail.com>
2022-06-05 17:56:13 +0200

    [#3]: Represent signed int in binary

---
+++ src/includes/kata/StringUtils.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "export.h"
+
+#include <string>
+
+namespace kata {
+
+KATA_EXPORT std::string intToBinaryRepresentation(int value);
+
+} // namespace kata
--- src/src/Source.cpp
+++
@@ -1,6 +0,0 @@
-#include "export.h"
-
-KATA_EXPORT int func()
-{
-    return 1;
-}
